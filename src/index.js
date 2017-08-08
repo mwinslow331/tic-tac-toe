d0540d931bd9f4e96f4e46e495fccaa29bd34a4f
mwinslow331 <michael.winslow331@gmail.com>
2017-08-08 12:27:55 -0400

    created board

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '.index/css';
+import Board from './Board';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+)
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
